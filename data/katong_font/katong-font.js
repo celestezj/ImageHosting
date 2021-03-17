f177ccaeb80857d483686edd60d3eadf597eb761
muggledy <3101266674@qq.com>
2021-03-17 21:12:10 +0800

    add katong-font.js

---
+++ data/katong_font/katong-font.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function dyformatContent(content, container) {
+	  var contentArray = content.split(' ');
+	  var formattedContent = document.createElement('div');
+	  //formattedContent.style.textAlign="center";
+	  contentArray.map(function (word) {
+		formattedContent.appendChild(dycreateWord(word));
+	  });
+	  console.log(contentArray);
+
+	  container.replaceChild(formattedContent, container.firstChild);
+};
+
+function dycreateWord(characters) {
+	  var word = document.createElement('div');
+	  var wordArray = characters.split('');
+	  wordArray.map(function (char) {
+		word.appendChild(dyformatCharacter(char));
+	  });
+	  //word.appendChild(formatCharacter('&nbsp;'));
+	  return word;
+}
+
+function dyformatCharacter(text) {
+	  var text = text === ' ' ? '&nbsp;' : text;
+	  var character = document.createElement('span');
+	  character.innerHTML = text;
+	  return character;
+}
+
+function dykatongrender(){
+	var dykatongallcontents=document.getElementsByClassName("dykatongcontent");
+	for(var i=0; i<dykatongallcontents.length; i++){
+		var dykatongcontent=dykatongallcontents[i].innerText;
+		dyformatContent(dykatongcontent, dykatongallcontents[i]);
+	}
+}
