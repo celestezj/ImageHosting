5f8d81b4c1cfda533f735a45031637bc5ff080b9
muggledy <3101266674@qq.com>
2021-03-22 20:43:29 +0800

    add funny-title-js.js

---
+++ data/funny_title/funny-title-js.js
@@ -0,0 +1,19 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        var dytitleicon = document.querySelector('link[rel*="icon"]');
+        dytitleicon.setAttribute('href', "https://cdn.jsdelivr.net/gh/celestezj/ImageHosting@v0.1/img/20210207091812.png");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        var dytitleicon = document.querySelector('link[rel*="icon"]');
+        dytitleicon.setAttribute('href', "https://cdn.jsdelivr.net/gh/celestezj/ImageHosting@v0.1/img/20210207091812.png");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
