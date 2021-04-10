a82b0a1c0896cd392334018bac6dcb2e675a3644
muggledy <3101266674@qq.com>
2021-04-10 13:38:41 +0800

    modify article_img_preview.js

--- data/img_preview/article_img_preview.js
+++ data/img_preview/article_img_preview.js
@@ -1,16 +1,17 @@
 /*
- * Image preview script 
+ * Image preview script
  * powered by jQuery (http://www.jquery.com)
  * 
  * written by Alen Grakalic (http://cssglobe.com)
  * 
  * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
- *
+ * 
+ * Applied in Butterfly by muggledy on 2021/4/9
  */
- 
+
 function image_preview_wrapper(){
 
-this.imagePreview = function(){	
+this.imagePreview = function(){
 	/* CONFIG */
 		
 		xOffset = 10;
@@ -23,11 +24,11 @@ this.imagePreview = function(){
 	$("a.article-sort-item-img").hover(function(e){
 		this.t = this.title;
 		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$("body").append("<div id='article-img-preview'><img src='"+ $(this).find('img').attr('src') +"' alt='Article Image Preview' style='width:300px;' />"+ c +"</div>");								 
+		$("body").append("<div id='article-img-preview'><img src='"+ $(this).find('img').attr('src') +"' alt='Article Image Preview' style='width:300px;' />"+ c +"</div>");
 		$("#article-img-preview")
 			.css("top",(e.pageY - xOffset) + "px")
 			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");						
+			.fadeIn("fast");
     },
 	function(){
 		$("#article-img-preview").remove();
@@ -36,9 +37,8 @@ this.imagePreview = function(){
 		$("#article-img-preview")
 			.css("top",(e.pageY - xOffset) + "px")
 			.css("left",(e.pageX + yOffset) + "px");
-	});			
+	});
 };
-
 
 // starting the script on page load
 $(document).ready(function(){
@@ -47,16 +47,12 @@ $(document).ready(function(){
 
 }
 
-if (typeof(btf)!="undefined"){
-	btf.isJqueryLoad(image_preview_wrapper);
-} else {
-	if(window.attachEvent){
-		window.attachEvent("onload",function() {
-			btf.isJqueryLoad(image_preview_wrapper);
-		});
-	}else{
-		window.addEventListener("load",function() {
-			btf.isJqueryLoad(image_preview_wrapper);
-		},true);
+//pjax响应完成后，删除上个页面（可能）残留的article-img-preview节点
+window.addEventListener('pjax:complete', function () {
+	var self = document.getElementById('article-img-preview'); // 拿到待删除节点
+	if (self) {
+		var parent = self.parentElement; // 拿到父节点
+		var removed = parent.removeChild(self); // 删除
+		//removed === self; // true //注意到删除后的节点虽然不在文档树中了，但其实它还在内存中，可以随时再次被添加到别的位置
 	}
-}
+});
