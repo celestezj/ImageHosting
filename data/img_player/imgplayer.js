a87bd98f4ee9cf1a387e43a7e66dfe2502024908
muggledy <zgjsycfndy2015@163.com>
2021-10-02 11:43:23 +0800

    update imgplayer.js

--- data/img_player/imgplayer.js
+++ data/img_player/imgplayer.js
@@ -3,19 +3,22 @@
 function SlideShowPlayer(id_name) {
     var rotator=document.getElementById(id_name);
     var images=rotator.getElementsByTagName('img');
+    var progress=rotator.parentNode.getElementsByClassName("imgplayer_progress")[0];
     var intervalID;
     var counter=1;
+    var baseprogress="/"+(images.length).toString();
 
     for (var i=1; i<images.length; i++) { //初始时刻隐藏除第一张外的所有图片（静止状态），也可以设置为预播放状态
         images[i].style.display='none';
     }
-
+    progress.innerText="1"+baseprogress;
     function animate() {
         var i;
 
         for (i=0; i<images.length; i++) {
             if (i==counter){
                 images[i].style.display='block';
+                progress.innerText=(counter+1).toString()+baseprogress;
             } else{
                 images[i].style.display='none';
             }
@@ -40,9 +43,13 @@ function SlideShowPlayer(id_name) {
         for (var i=0; i<images.length; i++) {
             images[i].style.display='none';
         }
+        rotator.style.display='none';
+        progress.style.display="none";
     };
 
     this.play=function () {
+        rotator.style.display='block';
+        progress.style.display="inline";
         if (undefined===intervalID) {
             intervalID=window.setInterval(animate, 500);
         }
