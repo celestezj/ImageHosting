2d84f0a8eb40898adb33cd1199485b5259e0eec7
muggledy <zgjsycfndy2015@163.com>
2022-05-18 18:16:34 +0800

    fix funny_title, img_player demo

--- data/funny_title/funny-title-js.js
+++ data/funny_title/funny-title-js.js
@@ -4,13 +4,13 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         var dytitleicon = document.querySelector('link[rel*="icon"]');
-        dytitleicon.setAttribute('href', "https://cdn.jsdelivr.net/gh/celestezj/ImageHosting@v0.1/img/20210207091812.png");
+        dytitleicon.setAttribute('href', "https://cdn.staticaly.com/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
         var dytitleicon = document.querySelector('link[rel*="icon"]');
-        dytitleicon.setAttribute('href', "https://cdn.jsdelivr.net/gh/celestezj/ImageHosting@v0.1/img/20210207091812.png");
+        dytitleicon.setAttribute('href', "https://cdn.staticaly.com/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
