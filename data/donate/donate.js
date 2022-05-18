83cb813b69b8b21a05cab031f18eee8dcb119083
muggledy <zgjsycfndy2015@163.com>
2022-05-18 17:59:08 +0800

    fix donate

--- data/donate/donate.js
+++ data/donate/donate.js
@@ -2,9 +2,9 @@ function exec_donate_func(){
 jQuery(document).ready(function() {
 	var QRBox	=	$('#QRBox');
 	var MainBox	=	$('#DonateMainBox');
-	var BTCQR	=	'https://cdn.jsdelivr.net/gh/celestezj/ImageHosting/data/donate/img/BTCQR.png';	// 二维码路径
-	var AliPayQR	=	'https://cdn.jsdelivr.net/gh/celestezj/ImageHosting/data/donate/img/AliPayQR.png';
-	var WeChanQR	=	'https://cdn.jsdelivr.net/gh/celestezj/ImageHosting/data/donate/img/WeChanSQ.png';
+	var BTCQR	=	'https://cdn.staticaly.com/gh/celestezj/ImageHosting/master/data/donate/img/BTCQR.png';	// 二维码路径
+	var AliPayQR	=	'https://cdn.staticaly.com/gh/celestezj/ImageHosting/master/data/donate/img/AliPayQR.png';
+	var WeChanQR	=	'https://cdn.staticaly.com/gh/celestezj/ImageHosting/master/data/donate/img/WeChanSQ.png';
 
 	function showQR(QR) {
 		if (QR) {
