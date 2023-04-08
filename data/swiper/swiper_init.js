026aa9686c1aca45083a66a3e1c2a07c9a2e2ad5
muggledy <zgjsycfndy2015@163.com>
2023-04-09 02:13:44 +0800

    update swiper_init.js

--- data/swiper/swiper_init.js
+++ data/swiper/swiper_init.js
@@ -12,6 +12,14 @@ var swiper = new Swiper('.blog-slider', {
   pagination: {
     el: '.blog-slider__pagination',
     clickable: true,
+  },
+  observer: true,
+  on: {
+    resize: function () {
+      setTimeout(() => {
+        swiper.update()
+      }, 500) //延时器很重要，否则因浏览器宽度（F12）或侧边栏显隐导致的所在div变化而重新计算的自适应宽度会有误，swiper显示就无法正常居中。此前分类页面的charts也是如此，只不过当时为了追求极致的平滑，担心定时interval太长（会有明显延迟，此处swiper即是如此，但也无伤大雅）太短都不准确，于是会以小interval做多次resize，更耗CPU、考验计算机性能
+    }
   }
 });
 
