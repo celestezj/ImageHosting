291f13b0fd46bd6f30f97fae5825f31e12c4fd10
muggledy <zgjsycfndy2015@163.com>
2023-09-17 08:01:48 +0800

    replace cdn.staticaly.com with cdn.statically.io

--- data/funny_title/funny-title.js
+++ data/funny_title/funny-title.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.staticaly.com/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
+        $('[rel="icon"]').attr('href', "https://cdn.statically.io/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.staticaly.com/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
+        $('[rel="icon"]').attr('href', "https://cdn.statically.io/gh/celestezj/ImageHosting/v0.1/img/20210207091812.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
